67bac6c3f5d22de02ebcc9e861fced5adf2c5e6a
scud3g <32032615+scud3g@users.noreply.github.com>
2021-04-25 12:11:02 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -1,4 +1,4 @@
-invite_userids = [244668941]
+invite_userids = [267067778,348370520]
 
 import json, os, time
 import requests
@@ -20,13 +20,13 @@ sids = [
 ]
 mk = 0
 
-def request_re(sid, invite_userid, rep = 30):
+def request_re(sid, invite_userid, rep = 80):
     invite_url = 'http://zt.wps.cn/2018/clock_in/api/invite'
     r = requests.post( invite_url, headers={ 'sid': sid }, data={ 'invite_userid': invite_userid, "client_code": "040ce6c23213494c8de9653e0074YX30", "client": "alipay" } )
     js = json.loads(r.content)
     if js['msg'] == 'tryLater' and rep > 0:
         rep -= 1
-        time.sleep(10)
+        time.sleep(20)
         r = request_re(sid, invite_userid, rep)
     return r
 
